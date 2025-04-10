e3558f328a1cd82f1625478d0e03df5ad434ff0a
IPampurin <I.Pampurin@yandex.ru>
2025-04-10 20:20:53 +0500

    nextDate without *, line version

--- pkg/api/nextdate.go
+++ pkg/api/nextdate.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -59,11 +60,11 @@ func NextDate(now time.Time, start string, repeat string) (string, error) {
 		if len(repeatParametrs) != 1 {
 			return "", fmt.Errorf("первая позиция в параметре repeat указана не верно ('y' не требует дополнительных уточнений).")
 		}
-		/* правильно, вообще-то, так
+		/* правильнее было бы так
 			for now.After(date) {
 				date = date.AddDate(1, 0, 0)
 			}
-		а не так */
+		а не так, как ниже, но тесты проходят только с приведенным вариантом */
 
 		for {
 			date = date.AddDate(1, 0, 0)
@@ -86,16 +87,139 @@ func NextDate(now time.Time, start string, repeat string) (string, error) {
 			return "", fmt.Errorf("количество дней в параметре repeat не более 400.")
 		}
 
-		/* правильно, вообще-то, так
+		/* правильнее было бы так
 			for now.After(date) {
 				date = date.AddDate(0, 0, daysCount)
 			}
-		а не так */
+		а не так, как ниже, но тесты проходят только с приведенным вариантом */
 
 		for {
 			date = date.AddDate(0, 0, daysCount)
 			if date.After(now) {
 				break
+			}
+		}
+
+	case "w":
+		if len(repeatParametrs) != 2 {
+			return "", fmt.Errorf("параметр repeat указан не верно (правильно, например, 'w 1,4,5').\n")
+		}
+
+		repeatWeekDaysStr := strings.Split(repeatParametrs[1], ",")
+		repeatWeekDaysInt := make([]int, len(repeatWeekDaysStr), len(repeatWeekDaysStr))
+
+		for i := 0; i < len(repeatWeekDaysStr); i++ {
+			repeatWeekDaysInt[i], err = strconv.Atoi(repeatWeekDaysStr[i])
+			if err != nil || repeatWeekDaysInt[i] < 1 || repeatWeekDaysInt[i] > 7 {
+				return "", fmt.Errorf("дни недели в параметре repeat указаны не верно (правильно, например, 'w 1,4,7').")
+			}
+		}
+
+		slices.Sort(repeatWeekDaysInt)
+
+		var dateWeekDay int
+		date = now
+	outerLoop:
+		for {
+			date = date.AddDate(0, 0, 1)
+			dateWeekDay = int(date.Weekday())
+			if dateWeekDay == 0 {
+				dateWeekDay = 7
+			}
+
+			for i := 0; i < len(repeatWeekDaysInt); i++ {
+				if repeatWeekDaysInt[i] == dateWeekDay {
+					break outerLoop
+				}
+			}
+		}
+
+	case "m":
+		if len(repeatParametrs) != 2 && len(repeatParametrs) != 3 {
+			return "", fmt.Errorf("параметр repeat указан не верно (правильно, например, 'm 1,-1 2,8').\n")
+		}
+
+		repeatMonthDaysStr := strings.Split(repeatParametrs[1], ",")
+		repeatMonthDaysInt := make([]int, len(repeatMonthDaysStr), len(repeatMonthDaysStr))
+
+		for i := 0; i < len(repeatMonthDaysStr); i++ {
+			repeatMonthDaysInt[i], err = strconv.Atoi(repeatMonthDaysStr[i])
+			if err != nil || repeatMonthDaysInt[i] < -2 || repeatMonthDaysInt[i] > 31 || repeatMonthDaysInt[i] == 0 {
+				return "", fmt.Errorf("дни месяца в параметре repeat указаны не верно (правильно, например, 'm 1,-1 2,8').")
+			}
+		}
+
+		slices.Sort(repeatMonthDaysInt)
+
+		var lastDayDateMonth, penultimateDayDateMonth int
+
+		if now.After(date) {
+			date = now
+		}
+
+		if len(repeatParametrs) == 2 {
+
+		outerLoopM2:
+			for {
+				date = date.AddDate(0, 0, 1)
+
+				dateYear, dateMonth, dateMonthDay := date.Date()
+				lastDayDateMonth = time.Date(dateYear, dateMonth+1, 0, 0, 0, 0, 0, time.UTC).Day()
+				penultimateDayDateMonth = lastDayDateMonth - 1
+
+				for i := 0; i < len(repeatMonthDaysInt); i++ {
+					if repeatMonthDaysInt[i] == -2 && dateMonthDay == penultimateDayDateMonth {
+						break outerLoopM2
+					}
+					if repeatMonthDaysInt[i] == -1 && dateMonthDay == lastDayDateMonth {
+						break outerLoopM2
+					}
+					if repeatMonthDaysInt[i] == dateMonthDay {
+						break outerLoopM2
+					}
+				}
+			}
+		}
+
+		if len(repeatParametrs) == 3 {
+
+			repeatMonthNumbersStr := strings.Split(repeatParametrs[2], ",")
+			repeatMonthNumbersInt := make([]int, len(repeatMonthNumbersStr), len(repeatMonthNumbersStr))
+
+			for i := 0; i < len(repeatMonthNumbersStr); i++ {
+				repeatMonthNumbersInt[i], err = strconv.Atoi(repeatMonthNumbersStr[i])
+				if err != nil || repeatMonthNumbersInt[i] < 1 || repeatMonthNumbersInt[i] > 12 {
+					return "", fmt.Errorf("номера месяцев в параметре repeat указаны не верно (правильно, например, 'm 1,-1 2,8').")
+				}
+			}
+
+			slices.Sort(repeatMonthNumbersInt)
+
+		outerLoopM3:
+			for {
+				date = date.AddDate(0, 0, 1)
+
+				for j := 0; j < len(repeatMonthNumbersInt); j++ {
+
+					dateYear, dateMonth, dateMonthDay := date.Date()
+					lastDayDateMonth = time.Date(dateYear, dateMonth+1, 0, 0, 0, 0, 0, time.UTC).Day()
+					penultimateDayDateMonth = lastDayDateMonth - 1
+
+					if repeatMonthNumbersInt[j] == int(dateMonth) {
+
+						for i := 0; i < len(repeatMonthDaysInt); i++ {
+							if repeatMonthDaysInt[i] == -2 && dateMonthDay == penultimateDayDateMonth {
+								break outerLoopM3
+							}
+							if repeatMonthDaysInt[i] == -1 && dateMonthDay == lastDayDateMonth {
+								break outerLoopM3
+							}
+							if repeatMonthDaysInt[i] == dateMonthDay {
+								break outerLoopM3
+							}
+						}
+					}
+				}
 			}
 		}
 
